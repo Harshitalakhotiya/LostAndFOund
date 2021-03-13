ef0fc9420789275a471ecac5ae62e0213e6551e1
TanShets <shettytanish02@gmail.com>
2021-03-14 02:40:54 +0530

    Corrected forwarding of coordinates in product

--- found/views.py
+++ found/views.py
@@ -11,17 +11,22 @@ def home(request):
 
 @login_required
 def found_form(request):
+	print(request.session)
 	if request.method == 'POST':
+		#print(request.POST.get('x'))
 		form = ProductFoundForm(request.POST)
 		if form.is_valid():
 			form.instance.person = request.user
-			this_task = form.save()
+			this_item = form.save()
+			this_item.x = request.session.get('x', 0.0)
+			this_item.y = request.session.get('y', 0.0)
+			this_item.save()
 
 			messages.success(request,f'Item is successfully added')
 			context = {
-				'id': this_task.id,
+				'id': this_item.id,
 			}
-			return render(request, 'found/map.html', context)
+			return redirect('l&f-home')
 		else:
 			form = ProductFoundForm()
 			title = "Register Missing Article"
@@ -46,7 +51,11 @@ def index(request):
 			'x'  : xv,
 			'y' : yv
 		}
-		return render(request, 'foundform', context)
+
+		request.session['x'] = xv
+		request.session['y'] = yv
+		#return render(request, 'foundform', context)
+		return redirect('foundform')
 		# return JsonResponse({"xvar":xv,"yvar":yv})
 	else:
 		return render(request, 'found/map.html')
