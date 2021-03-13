fdb88b628a0bfac83b5b1a98bc13f5769f8e313a
TanShets <shettytanish02@gmail.com>
2021-03-13 13:04:50 +0530

    Corrected Indentation

--- found/models.py
+++ found/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Location(models.Model):
+	name = models.CharField(max_length = 200)
+
+class Stations(Location):
+	pass
+
+class BusStop(Location):
+	pass
+
+class Product(models.Model):
+	name = models.CharField(max_length = 200)
+
+class Item(models.Model):
+	name = models.CharField(max_length = 200)
+
+class Tags(models.Model):
+	name = models.CharField(max_length = 100)
+    prod_id = models.ForeignKey(Product, null = True, on_delete = models.CASCADE)
