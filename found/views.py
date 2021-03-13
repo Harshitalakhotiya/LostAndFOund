199120ee71fbec08de83586c08dd975a3b3154ab
TanShets <shettytanish02@gmail.com>
2021-03-13 16:34:30 +0530

    Added find_item and variable title

--- found/views.py
+++ found/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import *
 
 # Create your views h
 def home(request):
-	
-	return render(request, 'found/home.html')
+	title = "Welcome To Lost and Found"
+	return render(request, 'found/home.html', {'title': title})
 
 def found_form(request):
-	
-	form = ProductFoundForm()
-		
-	return render(request, 'found/foundform.html', {'form': form})
+	if request.method == 'POST':
+		form_data = {
+
+		}
+		#form = ProductFoundForm(request.POST.body)
+		#print(request.POST)
+		form = ProductFoundForm(request.POST)
+		return redirect('l&f-home')
+	else:
+		form = ProductFoundForm()
+		title = "Register Missing Article"
+		return render(request, 'found/foundform.html', {'form': form, 'title': title})
+
+def find_item(request):
+	title = "Find Lost Item"
+	return render(request, 'found/find_item.html', {'title': title})
