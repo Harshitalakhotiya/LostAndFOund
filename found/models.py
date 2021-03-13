e0a8192d87aeea7620e53f7780b03a1719234c05
TanShets <shettytanish02@gmail.com>
2021-03-13 13:28:52 +0530

    Registered models

--- found/models.py
+++ found/models.py
@@ -3,20 +3,16 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Location(models.Model):
-	name = models.CharField(max_length = 200)
-
-class Stations(Location):
-	pass
-
-class BusStop(Location):
-	pass
+    name = models.CharField(max_length = 200)
+    loc_type = models.CharField(max_length = 100)
 
 class Product(models.Model):
-	name = models.CharField(max_length = 200)
+    name = models.CharField(max_length = 200)
+    location_found = models.ForeignKey(Location, null = True, on_delete = models.CASCADE)
+    description = models.CharField(max_length = 300)
+    tags = models.CharField(max_length = 200)
 
-class Item(models.Model):
-	name = models.CharField(max_length = 200)
+    time_found = models.DateTimeField(auto_now_add = True)
 
-class Tags(models.Model):
-	name = models.CharField(max_length = 100)
-    prod_id = models.ForeignKey(Product, null = True, on_delete = models.CASCADE)
+    def __str__(self):
+        return name
