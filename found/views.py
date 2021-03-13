d4f0fa105d3f047022d858a1697e65bd4c80da37
TanShets <shettytanish02@gmail.com>
2021-03-14 03:03:25 +0530

    Improved Tags

--- found/views.py
+++ found/views.py
@@ -4,6 +4,26 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import *
 from django.contrib.auth.models import User
+import yake
+
+def extract_keywords(description):
+	language = "en"
+	max_ngram_size = 3
+	deduplication_thresold = 0.9
+	deduplication_algo = 'seqm'
+	windowSize = 1
+	numOfKeywords = 20
+
+	custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_thresold, dedupFunc=deduplication_algo, windowsSize=windowSize, top=numOfKeywords, features=None)
+	keywords = custom_kw_extractor.extract_keywords(description)
+	tags = [kw[0] for kw in keywords]
+	for i in keywords:
+		print(i)
+	
+	if len(tags) >= 5:
+		return tags[-5:]
+	else:
+		return tags
 
 def home(request):
 	title = "Welcome To Lost and Found"
@@ -20,6 +40,7 @@ def found_form(request):
 			this_item = form.save()
 			this_item.x = request.session.get('x', 0.0)
 			this_item.y = request.session.get('y', 0.0)
+			this_item.tags = ' '.join(extract_keywords(request.POST['description']))
 			this_item.save()
 
 			messages.success(request,f'Item is successfully added')
