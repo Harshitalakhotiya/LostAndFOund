00b01135cbcf1fd5227df869a33432383dcfb1c5
Harshita Lakhotiya <harshitalakhotiya123@gmail.com>
2021-03-13 13:02:33 +0530

    third commit

--- found/urls.py
+++ found/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from views import home
+from .views import home
 
 
 urlpatterns = [
-    path('',home, name='home'),
+    path('',views.home, name='l&f-home'),
     
 ]
